043664d3a7e6a7690440b512b8a01118e5b0446f
Ang Li <angli@angs-mbp.devices.brown.edu>
2019-11-21 14:33:14 -0500

    Committer: Ang Li <ang_li@brown.edu> On branch ang Changes to be committed: 	new file:   __pycache__/preprocess.cpython-37.pyc 	modified:   preprocess.py 	new file:   test.ipynb
    Add cleaning for sentences
    Split sentences and causes into list of words
    Add padding
    Add build_vocab and convert_to_id
    Add ipython test file

--- preprocess.py
+++ preprocess.py
@@ -2,126 +2,190 @@ import numpy as np
 import tensorflow as tf
 import numpy as np
 import re
+import string
 
+##########DO NOT CHANGE#####################
+PAD_TOKEN = "*PAD*"
+STOP_TOKEN = "*STOP*"
+START_TOKEN = "*START*"
+UNK_TOKEN = "*UNK*"
 # ##########DO NOT CHANGE#####################
-# PAD_TOKEN = "*PAD*"
-# STOP_TOKEN = "*STOP*"
-# START_TOKEN = "*START*"
-# UNK_TOKEN = "*UNK*"
-# FRENCH_WINDOW_SIZE = 14
-# ENGLISH_WINDOW_SIZE = 14
-# ##########DO NOT CHANGE#####################
 
-# def pad_corpus(french, english):
-# 	"""
-# 	DO NOT CHANGE:
-
-# 	arguments are lists of FRENCH, ENGLISH sentences. Returns [FRENCH-sents, ENGLISH-sents]. The
-# 	text is given an initial "*STOP*".  All sentences are padded with "*STOP*" at
-# 	the end.
-
-# 	:param french: list of French sentences
-# 	:param english: list of English sentences
-# 	:return: A tuple of: (list of padded sentences for French, list of padded sentences for English)
-# 	"""
-# 	FRENCH_padded_sentences = []
-# 	FRENCH_sentence_lengths = []
-# 	for line in french:
-# 		padded_FRENCH = line[:FRENCH_WINDOW_SIZE]
-# 		padded_FRENCH += [STOP_TOKEN] + [PAD_TOKEN] * (FRENCH_WINDOW_SIZE - len(padded_FRENCH)-1)
-# 		FRENCH_padded_sentences.append(padded_FRENCH)
-
-# 	ENGLISH_padded_sentences = []
-# 	ENGLISH_sentence_lengths = []
-# 	for line in english:
-# 		padded_ENGLISH = line[:ENGLISH_WINDOW_SIZE]
-# 		padded_ENGLISH = [START_TOKEN] + padded_ENGLISH + [STOP_TOKEN] + [PAD_TOKEN] * (ENGLISH_WINDOW_SIZE - len(padded_ENGLISH)-1)
-# 		ENGLISH_padded_sentences.append(padded_ENGLISH)
-
-# 	return FRENCH_padded_sentences, ENGLISH_padded_sentences
-
-# def build_vocab(sentences):
-# 	"""
-# 	DO NOT CHANGE
-
-#   Builds vocab from list of sentences
-
-# 	:param sentences:  list of sentences, each a list of words
-# 	:return: tuple of (dictionary: word --> unique index, pad_token_idx)
-#   """
-# 	tokens = []
-# 	for s in sentences: tokens.extend(s)
-# 	all_words = sorted(list(set([STOP_TOKEN,PAD_TOKEN,UNK_TOKEN] + tokens)))
-
-# 	vocab =  {word:i for i,word in enumerate(all_words)}
-
-# 	return vocab,vocab[PAD_TOKEN]
-
-# def convert_to_id(vocab, sentences):
-# 	"""
-# 	DO NOT CHANGE
-
-#   Convert sentences to indexed 
-
-# 	:param vocab:  dictionary, word --> unique index
-# 	:param sentences:  list of lists of words, each representing padded sentence
-# 	:return: numpy array of integers, with each row representing the word indeces in the corresponding sentences
-#   """
-# 	return np.stack([[vocab[word] if word in vocab else vocab[UNK_TOKEN] for word in sentence] for sentence in sentences])
-
-
+####################### IO and CLEANING ################
 def read_data(file_name):
-	"""
-	DO NOT CHANGE
+    """
+    DO NOT CHANGE
 
   Load text data from file
 
-	:param file_name:  string, name of data file
-	:return: list of sentences, each a list of words split on whitespace
+    :param file_name:  string, name of data file
+    :return: list of sentences, each a list of words split on whitespace
   """
-	text = []
-	with open(file_name, 'rt', encoding='latin') as data_file:
-		for line in data_file: text.append(line)
-	return text
+    text = []
+    with open(file_name, 'rt', encoding='latin') as data_file:
+        for line in data_file: text.append(line)
+    return text
+
+def clean_sentences(text):
+    """
+    Clean the text and return a list of sentences
+
+    :param text
+    :return: np array of sentences, a sentence is a list of words
+    """
+    sentences = []
+    for i, line in enumerate(text):
+        sentence = re.sub('<[^<]+>', "", line)
+
+        # Remove punctuations from causes
+        sentence = sentence.translate(str.maketrans('', '', string.punctuation))
+        # Remove digits from causes
+        sentence = sentence.translate(str.maketrans('', '', string.digits))
+
+        # Split sentence into a list of words
+        sentence = sentence.split()
+        sentences.append(sentence)
+
+    return sentences        
+
 
 def extract_and_clean_emotions(text):
 
-	emotions = []
-	for i, line in enumerate(text):
-		# Extracts all strings within tags. First one is the emotion of the sentence
-		cur_emotion = re.findall('<(.*?)>', line)[0]
+    emotions = []
+    for i, line in enumerate(text):
+        # Extracts all strings within tags. First one is the emotion of the sentence
+        cur_emotion = re.findall('<(.*?)>', line)[0]
 
-		# Remove emotion tags
-		text[i] = text[i][len(cur_emotion) + 2:-len(cur_emotion) - 4]
+        # Remove emotion tags
+        text[i] = text[i][len(cur_emotion) + 2:-len(cur_emotion) - 4]
 
-		emotions.append(cur_emotion)
+        emotions.append(cur_emotion)
 
-	return emotions
+    return emotions
 
 def extract_and_clean_causes(text):
 
-	causes = []
-	for i, line in enumerate(text):
-		# Extracts the cause from a sentence. Returns a list so index into first and only element
-		cur_cause = re.findall('<cause>(.*?)<\\\cause>', line)[0]
+    causes = []
+    for i, line in enumerate(text):
+        # Extracts the cause from a sentence. Returns a list so index into first and only element
+        cur_cause = re.findall('<cause>(.*?)<\\\cause>', line)[0]
 
-		# Remove tags from line
-		re.sub('<cause>', '', text[i])
-		re.sub('<\\\cause>', '', text[i])
+        # Remove tags from line
+        re.sub('<cause>', '', text[i])
+        re.sub('<\\\cause>', '', text[i])
 
-		causes.append(cur_cause)
+        # Remove punctuations from causes
+        cur_cause = cur_cause.translate(str.maketrans('', '', string.punctuation))
+        # Remove digits from causes
+        cur_cause = cur_cause.translate(str.maketrans('', '', string.digits))
 
-	return causes
+        # Split sentence into a list of words
+        cur_cause = cur_cause.split()
+        causes.append(cur_cause)
 
+    return causes
+    
+####################### IO and CLEANING ################
+
+###################### PADDING #########################
+def pad_sentences_and_causes(sentences, causes):
+    """
+    Pad the sentences and causes to same lengths
+    
+    :param sentences: a np array of sentences
+    :param causes: a np array of causes
+    :return: np array of padded sentences, np array of padded causes
+    """
+
+    sentence_padding_size = get_padding_size(sentences)
+    cause_padding_size = get_padding_size(causes)
+
+    padded_sentences = pad_corpus(sentences, sentence_padding_size)
+    padded_causes = pad_corpus(causes, cause_padding_size)
+
+    return padded_sentences, padded_causes
+
+def get_padding_size(sentences):
+    """
+    Determine the padding size given all the causes
+
+    :param sentences: list of sentences, each a list of words
+    :return: int, the length of longest sentence
+    """
+    max_length = 0
+    for sentence in sentences:
+        if len(sentence) > max_length:
+            max_length = len(sentence)
+    
+    return max_length
+
+def pad_corpus(sentences, padding_size):
+    """
+    Pad all the sentences
+
+    :param sentenses: a np array of sentences
+    :param padding_size: the padding size, equals to the length of the longest sentence
+    :return: a np array of padded sentences
+    """
+    padded_sentences = []
+
+    for line in sentences:
+        padded_sentence = line[:padding_size]
+        padded_sentence += [STOP_TOKEN] + [PAD_TOKEN] * (padding_size - len(padded_sentence)-1)
+        padded_sentences.append(padded_sentence)
+
+    padded_sentences = np.array(padded_sentences)
+    return padded_sentences
+
+###################### PADDING #########################
+
+################### BUILD VOCAB ########################
+
+def build_vocab(sentences):
+    """
+    Builds vocab from list of sentences
+
+    :param sentences:  list of sentences, each a list of words
+    :return: tuple of (dictionary: word --> unique index, pad_token_idx)
+    """
+    tokens = []
+    for s in sentences: tokens.extend(s)
+    all_words = sorted(list(set([STOP_TOKEN,PAD_TOKEN,UNK_TOKEN] + tokens)))
+
+    vocab =  {word:i for i,word in enumerate(all_words)}
+
+    return vocab,vocab[PAD_TOKEN]
+
+def convert_to_id(vocab, sentences):
+    """
+    Convert sentences to indexed 
+
+    :param vocab:  dictionary, word --> unique index
+    :param sentences:  list of lists of words, each representing padded sentence
+    :return: numpy array of integers, with each row representing the word indeces in the corresponding sentences
+    """
+    return np.stack([[vocab[word] if word in vocab else vocab[UNK_TOKEN] for word in sentence] for sentence in sentences])
+
+################### BUILD VOCAB ########################
+
+################## MAIN INTERFACES #####################
 def get_data(file_name):
 
-	text = read_data(file_name)
+    text = read_data(file_name)
 
-	emotions = extract_and_clean_emotions(text)
+    sentences = clean_sentences(text)
 
-	causes = extract_and_clean_causes(text)
+    emotions = extract_and_clean_emotions(text)
 
-	#### By end of this line, text is cleaned from tags and emotion-cause pairs are extracted ####
+    causes = extract_and_clean_causes(text)
 
-get_data('data.txt')
-	
+    #### By end of this line, text is cleaned from tags and emotion-cause pairs are extracted ####
+    sentences = np.array(sentences)
+    emotions = np.array(emotions)
+    causes = np.array(causes)
+    
+    # Padding
+    padded_sentences, padded_causes = pad_sentences_and_causes(sentences, causes)
+
+    return padded_sentences, emotions, padded_causes
+################## MAIN INTERFACES #####################
